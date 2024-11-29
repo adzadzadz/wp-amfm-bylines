7bb011039f5a40482c48589379b8398edc39c6e4
Adrian T. Saycon <adzbite@gmail.com>
2024-11-30 04:41:29 +0800

    UI control

--- public/js/amfm-bylines-public.js
+++ public/js/amfm-bylines-public.js
@@ -1,32 +1,26 @@
-(function( $ ) {
+(function ($) {
 	'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+	$(document).ready(function () {
+		console.log(amfmLocalize);
 
-})( jQuery );
+		// Hide columns if no data
+		if (amfmLocalize.author === '') {
+			$('#amfm-byline-col-author').hide();
+		}
+		if (amfmLocalize.editor === '') {
+			$('#amfm-byline-col-editor').hide();
+		}
+		if (amfmLocalize.reviewedBy === '') {
+			$('#amfm-byline-col-reviewer').hide();
+		}
+
+		// Adjust widths to fit 100% total
+		var visibleCols = $('.amfm-byline-col:visible').length;
+		if (visibleCols > 0) {
+			var newWidth = 100 / visibleCols + '%';
+			$('.amfm-byline-col').css('width', newWidth);
+		}
+	});
+
+})(jQuery);
