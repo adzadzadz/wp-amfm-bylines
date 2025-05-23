d118c3ebaed87ba29e3edb6d259b9048bdb47624
Adrian T. Saycon <adzbite@gmail.com>
2025-05-24 01:33:28 +0800

    v2.7.4

--- amfm-bylines.php
+++ amfm-bylines.php
@@ -16,7 +16,7 @@
  * Plugin Name:       AMFM Bylines
  * Plugin URI: https://github.com/adzadzadz/woocommerce-box
  * Description:       Byline Management
- * Version:           2.7.3
+ * Version:           2.7.4
  * Author:            Adrian T. Saycon
  * Author URI:        https://adzjo.online/adz/
  * License:           GPL2
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'AMFM_BYLINES_VERSION', '2.7.3' );
+define( 'AMFM_BYLINES_VERSION', '2.7.4' );
 
 /**
  * The code that runs during plugin activation.
